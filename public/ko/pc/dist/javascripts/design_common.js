d130f2980c9c99f0eb842b5184a4ad897dd44ba0
Pops <babotop11@naver.com>
2020-03-23 16:18:07 +0900

    gnb 진행중

--- public/ko/pc/dist/javascripts/design_common.js
+++ public/ko/pc/dist/javascripts/design_common.js
@@ -63,18 +63,37 @@ $window.load(function () {
 function layout() {
     var $gnb = $("#gnb");
     var $gnbTxtOpen = $("#gnbTxtOpen");
-
-    var $gnbDepth01 = $gnb.find(".gnb_depth01");
-    var $heavyTxt = $gnb.find(".heavy_txt");
+    var $gnbDepth01 = $gnb.find(".gnb_depth01"),
+        $1depthBtn = $gnbDepth01.find("> li > button, > li > a");
+    var $gnbDepth02 = $gnb.find(".gnb_depth02"),
+        $2depthBtn = $gnbDepth02.find("> li > button, > li > a");
+    var $gnbDepth03 = $gnb.find(".gnb_depth03");
 
     $gnbTxtOpen.click(function(){
-        TweenMax.to($gnbDepth01, .3, {width:250, ease:esStep});
-        TweenMax.to($heavyTxt, .3, {x:0});
+        if(!$gnb.hasClass("nav_open")){
+            $gnb.addClass("nav_open");
+            TweenMax.to($gnbDepth01, .3, {width:250, ease:esStep});
+        } else {
+            $gnb.removeClass("nav_open");
+            TweenMax.to($gnbDepth01, .3, {width:80, ease:esStep});
+        }
     });
-
-
-
-
+    //1뎁스 버튼 클릭
+    $1depthBtn.click(function(){
+        var _this = $(this);
+        $1depthBtn.removeClass("active");
+        _this.addClass("active");
+    });
+    //2뎁스 버튼 클릭
+    $2depthBtn.click(function(){
+        var _this = $(this);
+        if(!_this.hasClass("active")){
+            $2depthBtn.removeClass("active");
+            _this.addClass("active");
+        } else {
+            _this.removeClass("active");
+        }
+    });
 }
 
 
